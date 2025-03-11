0428ed54331265b970927ab03df34de4d6dedd10
MRSgkce <mursidegokce985@gmail.com>
2025-03-11 17:02:36 +0300

    tableviewda apideki bütün para birimleri gösterildi.

--- Currency Converter/ViewController.swift
+++ Currency Converter/ViewController.swift
@@ -15,10 +15,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var GBP: UILabel!
     @IBOutlet weak var CHF: UILabel!
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    var para = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource=self
+        tableView.delegate=self
         
     }
 
@@ -41,26 +46,51 @@ class ViewController: UIViewController {
                         do{
                             let jsonResponse = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! Dictionary<String,Any>
                             
-                            DispatchQueue.main.async {
+                            
+                            /*DispatchQueue.main.async {
                                 if let rates = jsonResponse["rates"] as? [String : Any]{
+                                    
                                     if let cad = rates["CAD"] as? Double{
                                         self.CAD.text = "CAD: \(cad)"
+                                        self.para.append("CAD: \(cad)")
                                         
                                     }
                                     if let chf = rates["CHF"] as? Double{
                                         self.CHF.text = "CHF: \(chf)"
+                                      //  para.append(chf)
                                     }
                                     if let gbp = rates["GBP"] as? Double{
                                         self.GBP.text = "GBP: \(gbp)"
+                                       // para.append(gbp)
                                     }
                                     if let jpy = rates["TRY"] as? Double{
                                         self.TRY.text = "TRY: \(jpy)"
+                                       //para.append(jpy)
                                     }
                                     if let usd = rates["USD"] as? Double{
                                         self.USD.text = "USD: \(usd)"
+                                        //para.append(usd)
                                     }
+                                    self.tableView.reloadData()*/
+                            
+                            DispatchQueue.main.async {
+                                if let rates = jsonResponse["rates"] as? [String: Any] {
+                                    self.para.removeAll() // Eski verileri temizleyelim (tekrar eklenmesin diye)
+                                    
+                                    for (currency, value) in rates {
+                                        if let rate = value as? Double {
+                                            let currencyString = "\(currency): \(rate)"
+                                            self.para.append(currencyString)
+                                        }
+                                    }
+                                    
+                                    // TableView'ı yenile
+                                    self.tableView.reloadData()
                                 }
                             }
+
+                                
+                            
                         }catch{
                             print(error)
                         }
@@ -75,3 +105,15 @@ class ViewController: UIViewController {
     
 
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        para.count
+    }
+    
+  
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = para[indexPath.row]
+        return cell
+    }
+}
